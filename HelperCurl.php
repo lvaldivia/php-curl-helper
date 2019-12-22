93db5e4d07d904c58e7ea35e1bc176e9ed4080d4
luis <valdivialuis1989@gmail.com>
2019-12-22 12:44:02 -0500

    post request and refactor user agent

--- HelperCurl.php
+++ HelperCurl.php
@@ -38,8 +38,7 @@ class HelperCurl {
             CURLOPT_CONNECTTIMEOUT => 10,
             CURLOPT_SSL_VERIFYPEER => false,
             CURLOPT_HTTPHEADER => $headers,
-            //we send the HTTP_USER_AGENT
-            CURLOPT_USERAGENT => isset($_SERVER["HTTP_USER_AGENT"]) ? $_SERVER['HTTP_USER_AGENT'] : "",
+            CURLOPT_USERAGENT => self::getUserAgent(),
             CURLOPT_HEADER => false
         );
         curl_setopt_array($ch, $options);
@@ -49,6 +48,10 @@ class HelperCurl {
         self::$status_code = self::getStatusCode($ch);
         curl_close($ch);
         return $response;
+    }
+
+    private static getUserAgent(){
+        return isset($_SERVER["HTTP_USER_AGENT"]) ? $_SERVER['HTTP_USER_AGENT'] : "";
     }
 
     /**
@@ -61,6 +64,36 @@ class HelperCurl {
 
     private static function getStatusCode($ch){
         return curl_getinfo($ch, CURLINFO_HTTP_CODE)
+    }
+
+
+    /**
+     * Performs a post request on the chosen link and the chosen parameters
+     * in the array.
+     *
+     * @param string $url
+     * @param array $fields
+     *
+     * @return string returns the content of the given url after post
+     */
+    public static function post($url, $fields = array() ,$headers = array()) {
+        $ch = curl_init();
+        //$fields['iprequest'] = $_SERVER[""];
+        $options = array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_CONNECTTIMEOUT => 150,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_HTTPHEADER => $headers,
+            CURLOPT_POSTFIELDS => http_build_query($fields),
+            CURLOPT_POST => true,
+            CURLOPT_USERAGENT => self::getUserAgent(),
+        );
+        curl_setopt_array($ch, $options);
+        $response = curl_exec($ch);
+        self::$status_code = self::getStatusCode();
+        curl_close($ch);
+        return $response;
     }
 
 }
