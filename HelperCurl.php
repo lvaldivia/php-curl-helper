0a78b9f6cd8b5ae6f3f8d42d35e36b8bd2b4a24c
luis <valdivialuis1989@gmail.com>
2019-12-22 12:37:20 -0500

    base class

---
+++ HelperCurl.php
@@ -0,0 +1,21 @@
+<?php 
+
+/**
+ * Helper class
+ *
+ * @author lvaldivia
+ * @version 1.0
+ */
+
+
+/**
+ * GET, POST, DELETE, PUT.
+ */
+class HelperCurl {
+    //public variable to store status code of each request
+    public static $status_code;
+    
+
+}
+
+?>
