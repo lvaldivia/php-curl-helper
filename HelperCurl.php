31fad85eabeb6b718a0b554f85b0dfd8ba232cb5
luis <valdivialuis1989@gmail.com>
2019-12-22 22:52:25 -0500

    put request

--- HelperCurl.php
+++ HelperCurl.php
@@ -101,6 +101,36 @@ class HelperCurl {
         return $response;
     }
 
+
+     /**
+     * Performs a put request on the chosen link and the chosen parameters
+     * in the array.
+     *
+     * @param string $url
+     * @param array $fields
+     *
+     * @return string with the contents of the site
+     */
+    public static function put($url, $fields = array()) {
+        $post_field_string = http_build_query($fields);
+        $ch = curl_init($url);
+
+        $options = array(
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_CONNECTTIMEOUT => 10,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_CUSTOMREQUEST => "PUT",
+            CURLOPT_POSTFIELDS => $post_field_string
+        );
+        curl_setopt_array($ch, $options);
+
+        $response = curl_exec($ch);
+        self::$status_code = self::getStatusCode();
+        curl_close($ch);
+
+        return $response;
+    }
+
 }
 
 ?>
